2769116b1d563cfc2e8ee8fec5feda2a874ed5d3
PashaLyashok <lyashok_pasha@mail.ru>
2021-06-25 15:21:39 +0300

    'rewriting Hello module to pl_task82'

---
+++ web/modules/custom/pl_task82/src/Controller/Task82.php
@@ -0,0 +1,14 @@
+<?php
+namespace Drupal\pl_task82\Controller;
+
+class Task82 {
+  
+  /**
+   * Returns a simple page.
+   */
+  public function content() {
+    return array(
+        '#markup' => 'Hello, World!',
+    );
+  }
+}
