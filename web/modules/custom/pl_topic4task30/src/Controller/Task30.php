8bea0aa41ee12c8f8d84bf1dc96aac2562c5f437
PashaLyashok <lyashok_pasha@mail.ru>
2021-06-30 15:00:21 +0300

    'added custom access'

--- web/modules/custom/pl_topic4task30/src/Controller/Task30.php
+++ web/modules/custom/pl_topic4task30/src/Controller/Task30.php
@@ -6,6 +6,8 @@
 namespace Drupal\pl_topic4task30\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
 
 class Task30 extends ControllerBase{
 
@@ -30,22 +32,21 @@ class Task30 extends ControllerBase{
   }
 
   /**
+   * Checks access for a specific request.
+   */
+  public function access(AccountInterface $account) {
+    self::checkTime();
+    self::checkRole();
+    return AccessResult::allowedIf(self::checkRole() != false && self::checkTime() == true);
+  }
+
+  /**
    * Returns a simple page with five nodes.
    */
   public function content() {
-    self::checkTime();
-    self::checkRole();
-
-    if ((self::checkRole() != false) && (self::checkTime() == true) ) {
-      return [
-        '#theme' => 'my_task30',
-        '#data' => 'Hello, Manager!',
-      ];
-    } else {
-      return [
-        '#type' => 'markup',
-        "#markup" => 'Sorry,403.',
-      ];
-    }
-  }
+    return [
+      '#theme' => 'my_task30',
+      '#data' => 'Hello, Manager!',
+    ];
+  } 
 }
