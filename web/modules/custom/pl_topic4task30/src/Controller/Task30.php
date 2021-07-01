5e8ea99858c118a02c180682759712ada7a0a1b2
PashaLyashok <lyashok_pasha@mail.ru>
2021-07-01 11:24:41 +0300

    'update controller Task30'

--- web/modules/custom/pl_topic4task30/src/Controller/Task30.php
+++ web/modules/custom/pl_topic4task30/src/Controller/Task30.php
@@ -8,6 +8,7 @@ namespace Drupal\pl_topic4task30\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResult;
+use Drupal\user\Entity\User;
 
 class Task30 extends ControllerBase{
 
@@ -15,14 +16,16 @@ class Task30 extends ControllerBase{
    * Checks access for a specific request.
    */
   public function access(AccountInterface $account) {
-    $current_user = \Drupal::currentUser();
-    $roles = $current_user->getRoles();
+    $uid = $account->id();
+    $user = User::load($uid);
     $request_time = \Drupal::time()
       ->getCurrentTime();
-    $manager_role = in_array('manager', $roles);
     $even_time = $request_time % 2;
-
-    return AccessResult::allowedIf($manager_role != false && $even_time == true);
+    $has_manager_role = $user->hasRole('manager');
+    $has_uuid = $user->uuid();
+    $cond_1 = $has_manager_role !== FALSE && $even_time === TRUE;
+    $cond_2 = !empty($has_uuid) && $even_time !== TRUE;
+    return AccessResult::allowedIf($cond_1 !== FALSE || $cond_2 !== FALSE);
   }
 
   /**
