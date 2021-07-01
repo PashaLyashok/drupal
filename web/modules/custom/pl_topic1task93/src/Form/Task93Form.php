6fd5e87cf8bca1ede5d8d77a12f2af5dd8fbd514
PashaLyashok <lyashok_pasha@mail.ru>
2021-07-01 13:24:37 +0300

    'begin topic1 task93'

---
+++ web/modules/custom/pl_topic1task93/src/Form/Task93Form.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\pl_topic1task93\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Defines a form that configures forms module settings.
+ */
+class Task93Form extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'pl_topic1task93_admin_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'pl_topic1task93.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('pl_topic1task93.settings');
+    $form['create_nodes'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Your message'),
+      '#default_value' => $config->get('variable_name'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('pl_topic1task93.settings')
+      ->set('variable_name', $form_state->getValue('create_nodes'))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
