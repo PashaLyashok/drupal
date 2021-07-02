45579001638ef40fa4cc9b1107344e2dfe57de41
PashaLyashok <lyashok_pasha@mail.ru>
2021-07-02 12:32:35 +0300

    'create $date as object of DrupalDateTime'

--- web/modules/custom/pl_topic1task94/src/Controller/Task94.php
+++ web/modules/custom/pl_topic1task94/src/Controller/Task94.php
@@ -6,22 +6,24 @@
 namespace Drupal\pl_topic1task94\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Datetime\DrupalDateTime;
 
-class Task94 extends ControllerBase{
+class Task94 extends ControllerBase {
 
   /**
    * Returns a simple page with five nodes.
    */
   public function content() {    
-    $date = date('m/d/Y H:i:s', \Drupal::time()
-      ->getCurrentTime());
+    $date = new DrupalDateTime();
+    $date = new \Drupal\Core\Datetime\DrupalDateTime(); 
+    $date->__toString();
     
     return [
         '#theme' => 'my_task94',
-        "#data" => $date,
+        "#data" => $date->format('d-m-Y'),
         '#attached' => [
             'library' => ['pl_topic1task94/my-lib'],
         ],
-      ];
+    ];
   }
 }
