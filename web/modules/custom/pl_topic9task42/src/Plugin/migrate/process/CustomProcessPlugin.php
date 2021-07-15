de58bf2a02e73e85f570a74284e3c7cf561d2b4e
PashaLyashok <lyashok_pasha@mail.ru>
2021-07-15 15:40:29 +0300

    'rewrite comments'

--- web/modules/custom/pl_topic9task42/src/Plugin/migrate/process/CustomProcessPlugin.php
+++ web/modules/custom/pl_topic9task42/src/Plugin/migrate/process/CustomProcessPlugin.php
@@ -5,8 +5,6 @@ namespace Drupal\pl_topic9task42\Plugin\migrate\process;
 use Drupal\migrate\ProcessPluginBase;
 
 /**
- * Return reversed string.
- *
  * @MigrateProcessPlugin(
  *   id = "custom_plugin"
  * )
@@ -14,14 +12,21 @@ use Drupal\migrate\ProcessPluginBase;
 class CustomProcessPlugin extends ProcessPluginBase {
 
   /**
-   * {@inheritdoc}
+   * Return concatenation firstname and reverse surname in string format.
    */
   public function transform($value, \Drupal\migrate\MigrateExecutableInterface $migrate_executable, \Drupal\migrate\Row $row, $destination_property) {
     if (is_array($value)) {
+      /**
+       * Reverse surname.
+       */
       $reverse_surname = strrev($value[1]);
+      $reverse_surname = strtolower($reverse_surname);
+      $reverse_surname = ucfirst($reverse_surname);
+      /**
+       * Concatenation firstname with '-' and surname.
+       */
       $concat = $value[0] . ' - ' . $reverse_surname;
       return $concat;
     }
   }
-
 }
