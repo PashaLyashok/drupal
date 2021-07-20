a83cc68bdcaff535db76224db88060de16a98068
PashaLyashok <lyashok_pasha@mail.ru>
2021-07-20 15:43:26 +0300

    '123'

--- web/modules/custom/pl_topic13task78/src/Controller/Task78.php
+++ web/modules/custom/pl_topic13task78/src/Controller/Task78.php
@@ -7,6 +7,8 @@ namespace Drupal\pl_topic13task78\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\user\Entity\User;
+use Drupal\Core\Cache\CacheBackendInterface;
+
 
 class Task78 extends ControllerBase {
 
@@ -15,13 +17,21 @@ class Task78 extends ControllerBase {
    */
   public function content() {
     $user_id = \Drupal::currentUser()->id();
-    \Drupal::cache()->set('my_cache_for_user_name_' . $user_id, $user_id, time()  + 300);
-    $uid = \Drupal::cache()->get('my_cache_for_user_name_' . $user_id)->data;
-    $user = User::load($uid);
-    $user_name = $user->name->getValue();
+    $user = User::load($user_id);
+    $user_name_arr = $user->name->getValue();
+      $user_name_str = $user_name_arr[0]['value'];
+    if (NULL != (\Drupal::cache()->get('my_cache_for_user_name_'. $user_id))) {
+      $tags = [
+        'user:' . $user_id,
+      ];
+      \Drupal::cache()->set('my_cache_for_user_name_'. $user_id, $user_name_str, CacheBackendInterface::CACHE_PERMANENT, $tags);
+      $current_user_name = \Drupal::cache()->get('my_cache_for_user_name_' . $user_id)->data;
+    } else {
+      $current_user_name = \Drupal::cache()->get('my_cache_for_user_name_' . $user_id)->data;
+    }
     
     return [
-      '#markup' => 'User name: ' . $user_name[0]['value'],
+      '#markup' => 'name: ' . $current_user_name,
     ];
   } 
 }
