1fcdc91fb0d489f858f309c36ac0bbff007e03f9
PashaLyashok <lyashok_pasha@mail.ru>
2021-07-21 10:55:09 +0300

    'added clear cache when users name is update'

--- web/modules/custom/pl_topic13task78/src/Controller/Task78.php
+++ web/modules/custom/pl_topic13task78/src/Controller/Task78.php
@@ -9,29 +9,27 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\user\Entity\User;
 use Drupal\Core\Cache\CacheBackendInterface;
 
-
 class Task78 extends ControllerBase {
 
   /**
    * Returns page with user name taked from cache.
    */
   public function content() {
+
     $user_id = \Drupal::currentUser()->id();
     $user = User::load($user_id);
     $user_name_arr = $user->name->getValue();
-      $user_name_str = $user_name_arr[0]['value'];
-    if (NULL != (\Drupal::cache()->get('my_cache_for_user_name_'. $user_id))) {
-      $tags = [
-        'user:' . $user_id,
-      ];
-      \Drupal::cache()->set('my_cache_for_user_name_'. $user_id, $user_name_str, CacheBackendInterface::CACHE_PERMANENT, $tags);
-      $current_user_name = \Drupal::cache()->get('my_cache_for_user_name_' . $user_id)->data;
-    } else {
-      $current_user_name = \Drupal::cache()->get('my_cache_for_user_name_' . $user_id)->data;
-    }
+    $user_name_str = $user_name_arr[0]['value'];
+    $tags = [
+      'user:'. $user_id,
+    ];
+    if (\Drupal::cache()->get('my_cache_for_user_name_' . $user_id) === FALSE) {
+      \Drupal::cache()->set('my_cache_for_user_name_' . $user_id, $user_name_str, CacheBackendInterface::CACHE_PERMANENT, $tags);
+    } 
+    $cache_user_name = \Drupal::cache()->get('my_cache_for_user_name_' . $user_id)->data;
     
     return [
-      '#markup' => 'name: ' . $current_user_name,
+      '#markup' => 'User name: ' . $cache_user_name,
     ];
-  } 
+  }
 }
