30f50c92c8cf67cfd2b8d682f42e20288134c140
PashaLyashok <lyashok_pasha@mail.ru>
2021-07-22 13:24:41 +0300

    'added settings form for filter'

--- web/modules/custom/pl_topic1task113/src/Plugin/Filter/CapitalizeFilter.php
+++ web/modules/custom/pl_topic1task113/src/Plugin/Filter/CapitalizeFilter.php
@@ -8,6 +8,7 @@ namespace Drupal\pl_topic1task113\Plugin\Filter;
 
 use Drupal\filter\Plugin\FilterBase;
 use Drupal\filter\FilterProcessResult;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * @Filter(
@@ -15,22 +16,35 @@ use Drupal\filter\FilterProcessResult;
  *  title = @Translation("Words are Capitalized"),
  *  description = @Translation("Make some words capitalized"),
  *  type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
+ *  settings = {
+ *    "word_for_capitalize" = "example",
+ *  }
  * )
  */
 class CapitalizeFilter extends FilterBase {
+  
+  /**
+   * Filter setting form.
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form['word_for_capitalize'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Search'),
+      '#default_value' => $this->settings['word_for_capitalize'],
+      '#maxlength' => 1024,
+      '#size' => 250,
+    ];
 
+    return $form;
+  }
   /**
    * Filter logic execution.
    */
-  public function process($text, $langcode)
-  {
+  public function process($text, $langcode) {
     $result = new FilterProcessResult($text);
-    $words_list = ['long', 'fact', 'a'];
-    for ($i = 0; $i < count($words_list); $i++) {
-      $text = str_replace($words_list[$i], ucfirst($words_list[$i]), $text);
-      $result->setProcessedText($text);
-    }
-  
+    $text = str_replace($this->settings['word_for_capitalize'], ucfirst($this->settings['word_for_capitalize']), $text);
+    $result->setProcessedText($text);
+
     return $result;
   }
 }
